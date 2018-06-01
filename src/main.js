dd2e779527edbd8077b3692790a64f274c55fb32
goddlts <54337467@qq.com>
2018-06-01 17:46:16 +0800

    '搭建项目首页'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// 程序的入口
 import Vue from 'vue';
+// 根组件
 import App from './App';
 import router from './router';
+
+// 导入bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入css模块
+import '@/assets/css/index.css';
 
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 
+// 注册Vue-Router插件
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
   ]
 });
