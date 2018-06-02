67778cce07bda26576c0073ba6502b8d06f9d912
goddlts <54337467@qq.com>
2018-06-02 15:12:41 +0800

    编辑英雄

--- src/views/heroes/edit.vue
+++ src/views/heroes/edit.vue
@@ -3,31 +3,65 @@
     <h2 class="sub-header">编辑英雄</h2>
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" value="xxx">
+        <label for="name">英雄名称</label>
+        <input v-model="formData.name" type="text" class="form-control" id="name" placeholder="英雄名称" value="xxx">
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value="xxx">
+        <label for="gender">英雄性别</label>
+        <input v-model="formData.gender" type="text" class="form-control" id="gender" placeholder="英雄性别" value="xxx">
       </div>
-      <div class="form-group">
-        <label for="exampleInputFile">File input</label>
-        <input type="file" id="exampleInputFile">
-        <p class="help-block">Example block-level help text here.</p>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Check me out
-        </label>
-      </div>
-      <button type="submit" class="btn btn-success">Submit</button>
+      <button @click.prevent="handleEdit" type="submit" class="btn btn-success">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        gender: ''
+      },
+      id: -1
+    };
+  },
+  created() {
+    // 获取url上id参数
+    this.id = this.$route.params.id;
+    // 加载数据
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      // 发送请求，获取当前要编辑的英雄对象
+      axios.get(`http://localhost:3000/heroes/${this.id}`)
+        .then((res) => {
+          if (res.status === 200) {
+            this.formData = res.data;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    handleEdit() {
+      axios.patch(`http://localhost:3000/heroes/${this.id}`, this.formData)
+        .then((res) => {
+          if (res.status === 200) {
+            // 修改成功
+            this.$router.push({
+              name: 'heroes'
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
 
--- src/views/heroes/list.vue
+++ src/views/heroes/list.vue
@@ -74,7 +74,7 @@ export default {
         })
         .catch((err) => {
           console.log(err);
-        })
+        });
     }
   }
 };
