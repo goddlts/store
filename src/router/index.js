0e7aecc4a0a9577f1dd6cac4cbc75672b02d0b77
goddlts <54337467@qq.com>
2018-06-02 09:52:57 +0800

    英雄列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+// 加载路由规则对应的组件
+import HeroesList from '@/views/heroes/list';
+
 // 注册Vue-Router插件
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // { name: 'home', path: '/', redirect: '/heroes'},
+    { name: 'home', path: '/', redirect: { name: 'heroes' }},
+    { name: 'heroes', path: '/heroes', component: HeroesList }
   ]
 });
